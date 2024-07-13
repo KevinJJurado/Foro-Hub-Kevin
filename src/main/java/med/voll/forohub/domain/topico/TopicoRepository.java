c9c3c38a7e63afae245bfa7965c61dcb3f715e32
KevinJurado <kjuradoe@gmail.com>
2024-07-13 08:59:17 -0500

    finished project

---
+++ src/main/java/med/voll/forohub/domain/topico/TopicoRepository.java
@@ -0,0 +1,8 @@
+package med.voll.forohub.domain.topico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
